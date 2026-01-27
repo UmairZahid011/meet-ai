a5a7b1d1e70f1b090f404073b12f8026ed73d095
Shehroz Barlas <shehrozbarlas799@gmail.com>
2026-01-28 02:38:01 +0500

    done

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,23 +1,23 @@
-import mysql from 'mysql2/promise'
-
-export const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'meetai',
-  socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
-})
-
-
 // import mysql from 'mysql2/promise'
 
 // export const pool = mysql.createPool({
-//   host: 'us-phx-web1439.main-hosting.eu',
-//   user: 'u588575896_meetairoot',
-//   password: 'Meetai@1122333',
-//   database: 'u588575896_meetai',
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-//   connectTimeout: 10000, // 10 seconds
-// });
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'root',
+//   database: 'meetai',
+//   socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+// })
+
+
+import mysql from 'mysql2/promise'
+
+export const pool = mysql.createPool({
+  host: 'us-phx-web1439.main-hosting.eu',
+  user: 'u588575896_meetairoot',
+  password: 'Meetai@1122333',
+  database: 'u588575896_meetai',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  connectTimeout: 10000, // 10 seconds
+});
